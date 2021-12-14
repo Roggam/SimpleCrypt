c01191f1f2cc52b9fda224a0073eb5cf50704f83
junior <junior@zipcodes-MacBook-Pro-5.local>
2021-12-14 13:27:54 -0500

    completed lab

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -1,4 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.Scanner;
 
 import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
@@ -19,7 +25,7 @@ public class ROT13 {
 
     public String crypt(String text) throws UnsupportedOperationException {
 
-        return shifter(text,13).toString();
+        return shifter(text, 13).toString();
     }
 
     public String encrypt(String text) {
@@ -29,20 +35,23 @@ public class ROT13 {
 
     public String decrypt(String text) {
 
-        return shifter(text,13).toString();
+        return shifter(text, 13).toString();
     }
 
-    public  String rotate(String s, Character c) {
-       StringBuilder stringBuilder = new StringBuilder();
-        int charVal = (int) c;
-        for (int i = c; i < c + s.length(); i++) {
-            stringBuilder.append((char)i);
+    public String rotate(String s, Character c) {
+        String str = "";
+        String[] arr = s.split("");
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i].equals(c.toString())) {
+                str = leftrotate(s, i);
+            }
         }
-        return stringBuilder.toString();
+        return str;
     }
 
 
-    public StringBuffer shifter (String text, int shift) {
+    public StringBuffer shifter(String text, int shift) {
         StringBuffer result = new StringBuffer();
 
         for (int i = 0; i < text.length(); i++) {
@@ -57,13 +66,48 @@ public class ROT13 {
                         shift - 65) % 26 + 65);
                 result.append(ch);
 
-            } else {//lowecase
+            } else {//lowercase
                 char ch = (char) (((int) text.charAt(i) +
                         shift - 97) % 26 + 97);
                 result.append(ch);
             }
         }
         return result;
+    }
+
+
+    public String leftrotate(String str, int d) {
+        String ans = str.substring(d) + str.substring(0, d);
+        return ans;
+    }
+
+
+
+    public String loadFile(){
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("sonnet18.txt").getFile());
+        StringBuilder result = new StringBuilder("");
+
+        try(Scanner scanner = new Scanner(file)){
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                result.append(line).append("\n");
+            }
+
+            scanner.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        return result.toString();
+    }
+    public void output(String string) throws FileNotFoundException {
+
+        FileOutputStream outputStream= new FileOutputStream("sonnet18.enc");
+        Formatter formatter = new Formatter(outputStream);
+        formatter.format(string);
+        formatter.flush();
+
     }
 
 
--- Crypto/src/main.java
+++ Crypto/src/main.java
@@ -1,20 +1,30 @@
-public class main {
-    public static void main(String[] args) {
-      ROT13 rot13 = new ROT13();
+import java.io.FileNotFoundException;
+import java.io.StringReader;
 
+public class main {
+    public static void main(String[] args) throws FileNotFoundException {
+        ROT13 rot13 = new ROT13();
+//TESTING
         String str1 = rot13.shifter("ABCDEF", Character.getNumericValue('D')).toString();
         String str2 = rot13.shifter(str1, 5).toString();
-      //  System.out.println(str1);
+        //  System.out.println(str1);
         //System.out.println(Character.getNumericValue('D'));
-        System.out.println(rot13.rotate("ABCDEF",'D'));
-        System.out.println((char)68);
+        String string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Character ch = 'N';
+        System.out.println(rot13.rotate(string, ch));
+        System.out.println((int) 'D');
 
-        for (int i = 65; i < 68; i++) {
-            System.out.println((char) i);
-        }
+//TESTING
 
 
-//        System.out.println(rot13.shifter("Why did the chicken cross the road?",13));
-//        System.out.println(rot13.shifter("Gb trg gb gur bgure fvqr!",13));
+        ROT13 rot131 = new ROT13();
+        String output = rot131.encrypt(rot13.loadFile());
+        rot131.output(output);
+
+
     }
+
+
+
+
 }
